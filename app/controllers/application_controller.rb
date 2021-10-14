edc0285acc449ebfdfd22892bc446ac807be690c
HowieKw <82329150+HowieKw@users.noreply.github.com>
2021-10-14 01:52:15 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,51 +3,51 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   
-  get '/' do
-    {message: "Hello World"}.to_json
-  end
+#   get '/' do
+#     {message: "Hello World"}.to_json
+#   end
   
-  post '/signup' do
-    if User.find_by(username: user_params["username"])
-      { message: "Username already exists. Please log-in."}.to_json
-    else
-    @user = User.create(user_params)
-    @user.to_json
-  end
-end
+#   post '/signup' do
+#     if User.find_by(username: user_params["username"])
+#       { message: "Username already exists. Please log-in."}.to_json
+#     else
+#     @user = User.create(user_params)
+#     @user.to_json
+#   end
+# end
 
 
 
-  post '/login' do
-    if User.find_by(username: user_params["username"])
+#   post '/login' do
+#     if User.find_by(username: user_params["username"])
 
-      @user = User.find_by(username: user_params["username"])
+#       @user = User.find_by(username: user_params["username"])
 
-      if @user.password == user_params["password"]
-        { user: @user, message: "Logged in successfully..."}.to_json
-      else
-        { message: "Username OR Password incorrect! Try again.." }.to_json
-      end
+#       if @user.password == user_params["password"]
+#         { user: @user, message: "Logged in successfully..."}.to_json
+#       else
+#         { message: "Username OR Password incorrect! Try again.." }.to_json
+#       end
 
-    else
-      { message: "User doesn't exist. Please sign-up..." }.to_json
-  end
-end
+#     else
+#       { message: "User doesn't exist. Please sign-up..." }.to_json
+#   end
+# end
 
-  private
-    def user_params
-        allowed_params = %w(username password)
-        params.select{ |param, value| allowed_params.include?(param) }
-    end
+#   private
+#     def user_params
+#         allowed_params = %w(username password)
+#         params.select{ |param, value| allowed_params.include?(param) }
+#     end
 
-    get '/users/reviews' do
-      @user = User.find_by(id: params["user_id"])
+#     get '/users/reviews' do
+#       @user = User.find_by(id: params["user_id"])
 
-      if @user
-        @reviews = @user.reviews
-        @reviews.to_json
-    end
-  end
+#       if @user
+#         @reviews = @user.reviews
+#         @reviews.to_json
+#     end
+#   end
 
 #   delete "/reviews/:id" do
 #     @reviews = Review.find_by(id: params[:id])
